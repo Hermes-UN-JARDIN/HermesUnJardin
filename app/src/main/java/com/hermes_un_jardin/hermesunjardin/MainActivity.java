568091083c9e1153ac2461b11acc45414d7b2ee0
walfud <walfud@aliyun.com>
2015-05-18 14:12:42 +0800

    Action bar: Save + Edit

--- app/src/main/java/com/hermes_un_jardin/hermesunjardin/MainActivity.java
+++ app/src/main/java/com/hermes_un_jardin/hermesunjardin/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
 import com.hermes_un_jardin.hermesunjardin.view.NavDrawer;
@@ -22,6 +23,7 @@ public class MainActivity extends FragmentActivity {
     private ViewPager mMainBoard;
     private Fragment mCurrentFragment;
     private State mState = State.Default;
+    private Menu mMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,11 +88,14 @@ public class MainActivity extends FragmentActivity {
             }
         });
 
-        //
-        mDrawer.post(new Runnable() {
+        new View(this).post(new Runnable() {
             @Override
             public void run() {
+                // Close Drawer
                 mDrawer.setX(-mDrawer.getWidth());
+
+                // Init 'Setting Button' in Action Bar.
+                changeState(State.View);
             }
         });
     }
@@ -99,6 +104,7 @@ public class MainActivity extends FragmentActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        mMenu = menu;
         return true;
     }
 
@@ -123,6 +129,22 @@ public class MainActivity extends FragmentActivity {
                 mIsDrawerOpen = !mIsDrawerOpen;
                 break;
 
+            // State: View
+            case R.id.menu_edit:
+                changeState(State.Edit);
+                break;
+            case R.id.menu_drop:
+                break;
+            case R.id.menu_share:
+                break;
+
+            // State: Edit
+            case R.id.menu_save:
+                changeState(State.View);
+                break;
+            case R.id.menu_cancel:
+                break;
+
             default:
                 break;
         }
@@ -131,6 +153,36 @@ public class MainActivity extends FragmentActivity {
     }
 
     public void changeState(State state) {
+
+        MenuItem share = mMenu.findItem(R.id.menu_share);
+        MenuItem edit = mMenu.findItem(R.id.menu_edit);
+        MenuItem drop = mMenu.findItem(R.id.menu_drop);
+        MenuItem save = mMenu.findItem(R.id.menu_save);
+        MenuItem cancel = mMenu.findItem(R.id.menu_cancel);
+        switch (state) {
+            case Default:
+                break;
+
+            case View:
+                share.setVisible(true);
+                edit.setVisible(true);
+                drop.setVisible(true);
+                save.setVisible(false);
+                cancel.setVisible(false);
+                break;
+
+            case Edit:
+                share.setVisible(false);
+                edit.setVisible(false);
+                drop.setVisible(false);
+                save.setVisible(true);
+                cancel.setVisible(true);
+                break;
+
+            default:
+                break;
+        }
+
         ((PictureTextFragment) mCurrentFragment).setState(state);
         mState = state;
     }
